c3ce6b03337ccb2837acc7699f55c0fe26649b36
KarolKarnas <karol.karnas@gmail.com>
2023-05-09 16:52:23 +0200

    Solution for 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,10 +4,10 @@ const Button = ({ handleClick, text }) => {
 	return <button onClick={handleClick}>{text}</button>;
 };
 
-const Stats = ({ text, stats }) => {
+const Stats = ({ label, stats }) => {
 	return (
 		<p>
-			{text} {stats}
+			{label} {stats}
 		</p>
 	);
 };
@@ -16,22 +16,49 @@ const App = () => {
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
+	const [allReviews, setAllReviews] = useState([]);
 
-	const setToGood = () =>	setGood(good + 1);
-	const setToNeutral = () => setNeutral(neutral + 1);
-	const setToBad = () => setBad(bad + 1);
-
+	const incrementGood = () => {
+		setAllReviews((prevReviews) => [...prevReviews, 1]);
+		setGood(good + 1);
+	};
+	const incrementNeutral = () => {
+		setAllReviews((prevReviews) => [...prevReviews, 0]);
+		setNeutral(neutral + 1);
+	};
+	const incrementBad = () => {
+		setAllReviews((prevReviews) => [...prevReviews, -1]);
+		setBad(bad + 1);
+	};
 
 	return (
 		<div>
 			<h2>give feedback</h2>
-			<Button handleClick={setToGood} text={'good'} />
-			<Button handleClick={setToNeutral} text={'neutral'} />
-			<Button handleClick={setToBad} text={'bad'} />
+			<Button handleClick={incrementGood} text={'good'} />
+			<Button handleClick={incrementNeutral} text={'neutral'} />
+			<Button handleClick={incrementBad} text={'bad'} />
 			<h2>statistics</h2>
-			<Stats text={'good'} stats={good} />
-			<Stats text={'neutral'} stats={neutral} />
-			<Stats text={'bad'} stats={bad} />
+			<Stats label={'good'} stats={good} />
+			<Stats label={'neutral'} stats={neutral} />
+			<Stats label={'bad'} stats={bad} />
+			<Stats label={'total'} stats={allReviews.length} />
+			<Stats
+				label={'average'}
+				stats={
+					allReviews.length === 0
+						? 'N/A'
+						: allReviews.reduce((acc, current) => acc + current, 0) /
+						  allReviews.length
+				}
+			/>
+			<Stats
+				label={'positive'}
+				stats={
+					allReviews.length === 0
+						? 'N/A'
+						: `${(good / allReviews.length) * 100} %`
+				}
+			/>
 		</div>
 	);
 };

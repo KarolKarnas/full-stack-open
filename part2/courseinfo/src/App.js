c7983a5fd042ff8ffcb9b043c322d1d393fe0dbc
KarolKarnas <karol.karnas@gmail.com>
2023-05-11 12:25:28 +0200

    Solution for 2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,42 +1,4 @@
-const Header = ({ course }) => {
-	return <h2>{course.name}</h2>;
-};
-
-const Part = ({ name, exercises }) => {
-	return (
-		<p>
-			{name} {exercises}{' '}
-		</p>
-	);
-};
-
-const Total = ({ sumOfExercises }) => {
-	return <strong>Total of {sumOfExercises} exercises</strong>;
-};
-
-const Content = ({ course }) => {
-	const sumOfExercises = course.parts.reduce(
-		(acc, currentPart) => acc + currentPart.exercises,
-		0
-	);
-	return (
-		<div>
-			{course.parts.map((part) => (
-				<Part key={part.id} name={part.name} exercises={part.exercises} />
-			))}
-			<Total sumOfExercises={sumOfExercises} />
-		</div>
-	);
-};
-
-const Course = ({ course }) => {
-	return (
-		<>
-			<Header course={course} />
-			<Content course={course} />
-		</>
-	);
-};
+import Course from "./components/Course";
 
 const App = () => {
 

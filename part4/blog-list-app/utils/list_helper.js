3ff7bc9c04d9b72e90169fc3b681727457bb6e60
KarolKarnas <karol.karnas@gmail.com>
2023-06-05 13:53:05 +0200

    4.5*: helper functions and unit tests, step3

--- part4/blog-list-app/utils/list_helper.js
+++ part4/blog-list-app/utils/list_helper.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
 	return 1
 }
@@ -10,7 +11,24 @@ const totalLikes = (blogs) => {
 	return blogs.reduce(addLike, 0)
 }
 
+const favoriteBlog = (blogs) => {
+	if (blogs.length === 0) {
+		return 'there is no favorite blog here'
+	} else {
+		const { title, author, likes } = blogs.reduce((prev, curr) =>
+			curr.likes > prev.likes ? curr : prev
+		)
+
+		return {
+			title,
+			author,
+			likes,
+		}
+	}
+}
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 }

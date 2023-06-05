9ff4e4f9e6aed53fe78e84c1dc1b76391f7a3888
KarolKarnas <karol.karnas@gmail.com>
2023-06-05 08:14:45 +0200

    Clean code

--- part4/blog-list-app/index.js
+++ part4/blog-list-app/index.js
@@ -6,25 +6,26 @@ const Blog = require('./models/blog')
 
 const app = express()
 
-
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
 	logger.info(Blog)
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+	Blog.find({}).then((blogs) => {
+		response.json(blogs)
+	})
 })
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+	const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+	blog.save().then((result) => {
+		response.status(201).json(result)
+	})
 })
 
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}  - http://localhost:${config.PORT}`)
+	logger.info(
+		`Server running on port ${config.PORT}  - http://localhost:${config.PORT}`
+	)
 })

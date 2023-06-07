1ed747dec085b6d704756aaac430962468f30958
KarolKarnas <karol.karnas@gmail.com>
2023-06-07 11:42:03 +0200

    4.9: Blog list tests, step2

--- part4/blog-list-app/tests/blog_api.test.js
+++ part4/blog-list-app/tests/blog_api.test.js
@@ -16,46 +16,50 @@ const initialBlogs = [
 		author: 'Edsger W. Dijkstra',
 		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
 		likes: 5,
-	}
+	},
 ]
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+	await Blog.deleteMany({})
+	let blogObject = new Blog(initialBlogs[0])
+	await blogObject.save()
+	blogObject = new Blog(initialBlogs[1])
+	await blogObject.save()
 })
 
 test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+	await api
+		.get('/api/blogs')
+		.expect(200)
+		.expect('Content-Type', /application\/json/)
 })
 
 test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
+	const response = await api.get('/api/blogs')
 
-  expect(response.body).toHaveLength(initialBlogs.length)
+	expect(response.body).toHaveLength(initialBlogs.length)
 })
 
 test('a specific blog is within returned blogs', async () => {
-  const response = await api.get('/api/blogs')
+	const response = await api.get('/api/blogs')
 
-  const titles = response.body.map(blog => blog.title)
+	const titles = response.body.map((blog) => blog.title)
 
-  expect(titles).toContain('Go To Statement Considered Harmful')
+	expect(titles).toContain('Go To Statement Considered Harmful')
+})
+
+test('unique identifier property of the blog posts is named id for each blog', async () => {
+	const response = await api.get('/api/blogs')
+	response.body.forEach((blog) => expect(blog.id).toBeDefined())
+	// expect(response.body[0].id).toBeDefined()
 })
 
 // test('a valid blog can be added', async () => {
 //   const newBlog = 	{
-// 		_id: '5a422b3a1b54a676234d17f9',
 // 		title: 'Canonical string reduction',
 // 		author: 'Edsger W. Dijkstra',
 // 		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
 // 		likes: 12,
-// 		__v: 0,
 // 	}
 
 //   await api
@@ -75,5 +79,5 @@ test('a specific blog is within returned blogs', async () => {
 // })
 
 afterAll(async () => {
-  await mongoose.connection.close()
+	await mongoose.connection.close()
 })

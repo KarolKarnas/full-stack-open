a83becfd2ca867de1807bd665e935f3271221d92
KarolKarnas <karol.karnas@gmail.com>
2023-06-10 15:27:28 +0200

    4.17: bloglist expansion, step5

--- part4/blog-list-app/controllers/blogs.js
+++ part4/blog-list-app/controllers/blogs.js
@@ -1,23 +1,36 @@
 const blogsRouter = require('express').Router()
 // const logger = require('../utils/logger')
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-	const blogs = await Blog.find({})
+	const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
 	response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-	const { title, author, url, likes } = request.body
+	const { title, author, url, likes, userId } = request.body
+
+	let user
+	if (!userId) {
+		user = await User.findOne()
+	} else {
+		user = await User.findById(userId)
+	}
 
 	const blog = new Blog({
 		title,
 		author,
 		url,
 		likes,
+		user: user.id,
 	})
 
 	const savedBlog = await blog.save()
+
+	user.blogs = user.blogs.concat(savedBlog._id)
+	await user.save()
+
 	response.status(201).json(savedBlog)
 })
 
--- part4/blog-list-app/controllers/users.js
+++ part4/blog-list-app/controllers/users.js
@@ -3,7 +3,12 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-	const users = await User.find({})
+	const users = await User.find({}).populate('blogs', {
+		title: 1,
+		author: 1,
+		url: 1,
+		id: 1,
+	})
 	response.json(users)
 })
 
@@ -11,13 +16,21 @@ usersRouter.post('/', async (request, response) => {
 	const { username, name, password } = request.body
 
 	if (!password) {
-		response.status(400).json({ error: 'User validation failed: password: Path `password` is required.' })
+		response
+			.status(400)
+			.json({
+				error: 'User validation failed: password: Path `password` is required.',
+			})
 	}
 
-  if(password.length < 3) {
-    response.status(400).json({ error: 'User validation failed: password: Path `password` (`ad`) is shorter than the minimum allowed length (3).' })
-  }
-
+	if (password.length < 3) {
+		response
+			.status(400)
+			.json({
+				error:
+					'User validation failed: password: Path `password` (`ad`) is shorter than the minimum allowed length (3).',
+			})
+	}
 
 	const saltRounds = 10
 	const passwordHash = await bcrypt.hash(password, saltRounds)

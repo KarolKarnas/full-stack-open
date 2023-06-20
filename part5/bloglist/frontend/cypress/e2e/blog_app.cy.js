8bdac587104df190c82b28a9d012e51eb4dcedff
KarolKarnas <karol.karnas@gmail.com>
2023-06-20 14:00:18 +0200

    5.18: bloglist end to end testing, step2

--- part5/bloglist/frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist/frontend/cypress/e2e/blog_app.cy.js
@@ -1,11 +1,42 @@
-describe('Blog app', function() {
-  beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
-  })
+describe('Blog app', function () {
+	beforeEach(function () {
+		cy.request('POST', 'http://localhost:3003/api/testing/reset')
+		const user = {
+			name: 'Admin Karnas',
+			username: 'admin',
+			password: 'admin',
+		}
+		cy.request('POST', 'http://localhost:3003/api/users', user)
+		cy.visit('http://localhost:3000')
+	})
 
-  it('Login form is shown', function() {
-    cy.contains('Blog list')
-    cy.get('#login-form').should('be.visible')
-  })
+	it('Login form is shown', function () {
+		cy.contains('Blog list')
+		cy.get('#login-form').should('be.visible')
+	})
+
+	describe('Login', function () {
+		it('succeeds with correct credentials', function () {
+			cy.contains('log in').click()
+			cy.get('#username').type('admin')
+			cy.get('#password').type('admin')
+			cy.get('#login-button').click()
+
+			cy.contains('Admin Karnas logged in')
+		})
+
+		it('fails with wrong credentials, notification shown with unsuccessful login is displayed red', function () {
+			cy.contains('log in').click()
+			cy.get('#username').type('admin')
+			cy.get('#password').type('wrong')
+			cy.get('#login-button').click()
+
+      cy.get('.error')
+			.should('contain', 'invalid username or password')
+			.and('have.css', 'color', 'rgb(255, 0, 0)')
+			.and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Admin Karnas logged in')
+		})
+	})
 })
--- part5/bloglist/frontend/src/App.js
+++ part5/bloglist/frontend/src/App.js
@@ -146,6 +146,7 @@ const App = () => {
 			<div>
 				username
 				<input
+				id='username'
 					type='text'
 					value={username}
 					name='Username'
@@ -155,13 +156,14 @@ const App = () => {
 			<div>
 				password
 				<input
+				id='password'
 					type='password'
 					value={password}
 					name='Password'
 					onChange={({ target }) => setPassword(target.value)}
 				/>
 			</div>
-			<button type='submit'>login</button>
+			<button id='login-button' type='submit'>log in</button>
 		</form>
 	)
 

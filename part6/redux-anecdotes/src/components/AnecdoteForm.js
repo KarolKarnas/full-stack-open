8338a4a69e12892cf4e3dbaa1e27f8c21aea59ba
KarolKarnas <karol.karnas@gmail.com>
2023-06-26 13:50:31 +0200

    6.17 Anecdotes and the backend, step4

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,17 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { createNotification, removeNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
+// import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = async (e) => {
+  const addAnecdote = (e) => {
 		e.preventDefault()
     const content = e.target.anecdote.value
     e.target.anecdote.value = ''
-   const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
+  //  const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(content))
     dispatch(createNotification(`you created '${content}'`))
     setTimeout(() => {
 			dispatch(removeNotification())
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -5,9 +5,9 @@ const anecdoteSlice = createSlice({
 	name: 'anecdotes',
 	initialState: [],
 	reducers: {
-		createAnecdote(state, action) {
-state.push(action.payload)
-		},
+// 		createAnecdote(state, action) {
+// state.push(action.payload)
+// 		},
 		addVote(state, action) {
 			const id = action.payload
 			const anecdoteToVote = state.find((anecdote) => anecdote.id === id)
@@ -20,19 +20,29 @@ state.push(action.payload)
 				anecdote.id === id ? changedAnecdote : anecdote
 			)
 		},
+		appendAnecdote(state, action) {
+			state.push(action.payload)
+		},
 		setAnecdotes(state, action) {
 			return action.payload
 		}
 	},
 })
 
-export const {createAnecdote , addVote, setAnecdotes} = anecdoteSlice.actions
+export const { addVote, appendAnecdote, setAnecdotes} = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
 	return async dispatch => {
 		const anecdotes = await anecdotesService.getAll()
 		dispatch(setAnecdotes(anecdotes))
 	}
+}
+
+export const createAnecdote = (content) => {
+return async dispatch => {
+	const newAnecdote = await anecdotesService.createNew(content)
+	dispatch(appendAnecdote(newAnecdote))
+}
 }
 
 export default anecdoteSlice.reducer

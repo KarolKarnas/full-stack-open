4df844105bd61e35cbfa9ebedc1a0c295d75dd85
KarolKarnas <karol.karnas@gmail.com>
2023-07-16 14:57:01 +0200

    start working on 9.5 WebBMI

--- part9/bmi-calculator-ts/index.ts
+++ part9/bmi-calculator-ts/index.ts
@@ -5,6 +5,18 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
+app.get('/bmi', (req, res) => {
+  console.log(req.query)
+  const height = req.query.height
+  const weight = req.query.weight
+  res.send(`height = ${height}, weight= ${weight}`)
+  res.json({
+    weight,
+    height,
+    bmi: "Normal (healthy weight)"
+  })
+})
+
 const PORT = 3003;
 
 app.listen(PORT, () => {

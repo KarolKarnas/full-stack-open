fad3e7f3f9d1a96cd35bdb159b77162700544c27
KarolKarnas <karol.karnas@gmail.com>
2023-07-18 13:55:54 +0200

    9.11: Patientor backend, step4

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import diagnosesRouter from './routes/diagnoses';
+import patientsRouter from './routes/patients';
 import cors from 'cors';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 const PORT = 3001;
 

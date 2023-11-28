5c8f504aa410ae5592b851719b976a5af679e82a
KarolKarnas <karol.karnas@gmail.com>
2023-11-28 10:28:56 +0100

    Task 13.4.

--- part13/blog-postgres/index.js
+++ part13/blog-postgres/index.js
@@ -1,16 +1,73 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, QueryTypes, DataTypes, Model } = require('sequelize')
 const express = require('express')
 const app = express()
-
-
 const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+app.use(express.json())
+
+class Blog extends Model {}
+
+Blog.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+    author: {
+      type: DataTypes.TEXT
+    },
+		url: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		title: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+		likes: {
+			type: DataTypes.INTEGER,
+      defaultValue: 0
+		}
+	},
+	{
+		sequelize,
+		underscored: true,
+		timestamps: false,
+		modelName: 'blog',
+	}
+);
+
+Blog.sync()
 
 
 app.get('/api/blogs', async (req, res) => {
   const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
   res.json(blogs)
 })
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    console.log(blog.toJSON())
+    return res.json(blog)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  if (blog) {
+    await blog.destroy()
+    res.json(`${req.params.id} blog deleted successfully`);
+  } else {
+    res.status(404).end()
+  }
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)

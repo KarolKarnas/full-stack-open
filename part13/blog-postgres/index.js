d6dc5bb5a8c89d1aeb492976ce002eadaaf91f21
KarolKarnas <karol.karnas@gmail.com>
2023-11-29 12:12:12 +0100

    Exercise 13.9.

--- part13/blog-postgres/index.js
+++ part13/blog-postgres/index.js
@@ -10,13 +10,13 @@ const loginRouter = require('./controllers/login');
 
 const errorHandler = (error, request, response, next) => {
 	console.error(error.message);
-
 	if (error.name === 'TypeError') {
 		return response.status(400).send({ error: 'malformatted id' });
 	} else if (error.name === 'ValidationError') {
 		return response.status(400).json({ error: error.message });
-	}
-
+	} else if (error.name === 'SequelizeValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
 	next(error);
 };
 
